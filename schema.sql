c5395762ca3d9004d90ab5bd78f5e0878f486834
Robert Bovee <Rjbovee112@gmail.com>
2020-02-23 11:05:23 -0700

    tables created and added to mysql

--- schema.sql
+++ schema.sql
@@ -7,3 +7,20 @@ CREATE TABLE department (
     name VARCHAR (30) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id int AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
